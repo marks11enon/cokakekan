ffeecfc3164da3110cdb1ec4d91e730176fd3891
marks11enon <abcpandas11@gmail.com>
2022-04-02 06:18:27 +0000

    Add homes controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "2d91c13202734275b952504dcd8d1b08.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end

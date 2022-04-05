544a91476e0b52fa7ddc31bdbcad9e59869cb795
marks11enon <abcpandas11@gmail.com>
2022-04-05 16:05:10 +0000

    [Add]detail 明細入力、要カラム編集

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,4 +2,15 @@ class Detail < ApplicationRecord
   belongs_to :user
   belongs_to :month
   belongs_to :category
+
+  validates :user_id, presence: true
+  validates :month_id, presence: true
+  validates :category_id, presence: true
+  validates :player, presence: true
+  validates :status, presence: true
+  validates :date, presence: true
+  validates :note, length: { maximum: 50 }
+
+  # enum 設定
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :create, :edit, :destroy, :update]
   resources :details
   resources :budgets
-  resources :months
+  resources :months, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   get '/homes/about' => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

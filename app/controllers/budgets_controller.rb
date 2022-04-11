652d07f6a2bf7225c5348f638a323b7b19a99428
marks11enon <abcpandas11@gmail.com>
2022-04-11 09:35:57 +0000

    [Add]予算登録、予算一覧画面

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -4,14 +4,25 @@ class BudgetsController < ApplicationController
     @budget = Budget.new
   end
 
-  def index
+  def create
+    @budget = Budget.new(budget_params)
+    @budget.user_id = current_user.id
+    @budget.save
+    redirect_to budgets_path
   end
 
-  def show
+  def index
+    @budgets = Budget.all.order(month_id: :asc, id: :asc)
+  end
+
+  def destroy
+    @budget = Budget.find(params[:id])
+    @budget.delete
+    redirect_to budgets_path
   end
 
   private
   def budget_params
-    params.require(:budget).permit(:amount)
+    params.require(:budget).permit(:month_id, :amount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :categories, only: [:index, :create, :edit, :destroy, :update]
-  resources :budgets
+  resources :budgets, only: [:new, :create, :destroy, :index]
   resources :months, only: [:new, :create, :index, :show, :edit, :update, :destroy], shallow: true do
     resources :details, only: [:new, :create, :index, :edit, :update, :destroy]
   end
-  get '/details/:id/edit' => "details#edit"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

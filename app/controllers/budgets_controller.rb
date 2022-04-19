d000668a94834c8df68632b581d1b4585afd43c9
marks11enon <abcpandas11@gmail.com>
2022-04-19 16:27:24 +0000

    [Update] 予算登録ページ修正 rollback要確認

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -5,31 +5,35 @@ class BudgetsController < ApplicationController
   end
 
   def create
-    @add = Budget.new(budget_params)
+    @budgets = Budget.all # 追加
+    @budget = Budget.new(budget_params)
     if !this_month_budget
-      @add.user_id = current_user.id
-      @add.month_id = @month.id
+      @budget.user_id = current_user.id
+      @budget.month_id = @month.id
 
-      if @add.save
-        redirect_to budgets_path, success: "予算を登録しました。"
+      if @budget.save
+        # redirect_to budgets_path, success: "予算を登録しました。"
+        render :index
       else
-        redirect_to budgets_path, danger: "予算を設定できませんでした。"
+        # redirect_to budgets_path, danger: "予算を設定できませんでした。"
+        render :index
       end
     else
-      redirect_to budgets_path, danger: "予算は既に設定されています。"
+      # redirect_to budgets_path, danger: "予算は既に設定されています。"
+      render :index
     end
   end
 
   def index
     @budgets = Budget.all.order(month_id: :asc, id: :asc)
     @month = Month.find_by(user_id: current_user.id, month: Date.today.beginning_of_month)
-    @budget = Budget.new
+    @budget = Budget.new(budget_params)
   end
 
   def destroy
+    @budgets = Budget.all # 追加
     @budget = Budget.find(params[:id])
-    @budget.delete
-    redirect_to budgets_path
+    @budget.destroy
   end
 
   private
@@ -39,6 +43,6 @@ class BudgetsController < ApplicationController
 
   def this_month_budget
     @month = Month.find_by(user_id: current_user.id, month: Date.today.beginning_of_month)
-    @budget = Budget.find_by(user_id: current_user.id, month_id: @month.id)
+    @this_month_budget = Budget.find_by(user_id: current_user.id, month_id: @month.id)
   end
 end
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,8 @@
 class Budget < ApplicationRecord
   belongs_to :user
   belongs_to :month
+
+  validates :user_id, presence: true
+  validates :month_id, presence: true
+  validates :amount, presence: true
 end

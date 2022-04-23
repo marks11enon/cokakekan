78172ec5076fb559268e823d0b53827c962630a9
marks11enon <abcpandas11@gmail.com>
2022-04-23 18:55:55 +0000

    [Add] rspec model month

--- spec/factories/months.rb
+++ spec/factories/months.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :month do
-    
+    month { "#{Date.today.beginning_of_month}" }
+    balance { "150000" }
+    balance_last { "150000" }
   end
 end
--- spec/models/month_spec.rb
+++ spec/models/month_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Month, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+    @month = FactoryBot.build(:month)
+  end
+
+  describe '家計簿登録' do
+    it "全ての情報があれば登録できる" do
+      user = FactoryBot.create(:user)
+      month = FactoryBot.build(:month)
+      month.user_id = user.id
+      expect(month).to be_valid
+    end
+
+    it "ユーザーIDがなければ登録できない" do
+      expect(@month).to be_invalid
+    end
+
+    it "登録月がなければ登録できない" do
+      @month.month = nil
+      @month.user_id = @user.id
+      expect(@month).to be_invalid
+    end
+
+    it "口座残高がなければ登録できない" do
+      @month.month = nil
+      @month.user_id = @user.id
+      expect(@month).to be_invalid
+    end
+  end
 end

1f255cd19d25be3209c929f1c6dbcfefb21f4fb1
marks11enon <abcpandas11@gmail.com>
2022-06-23 13:43:22 +0000

    [Update] バリデーションエラー修正

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -11,8 +11,11 @@ class DetailsController < ApplicationController
     detail = Detail.new(detail_params)
     detail.user_id = current_user.id
     detail.month_id = params[:month_id]
-    detail.save
-    redirect_to month_details_path
+    if detail.save
+      redirect_to month_details_path
+    else
+      render "new"
+    end
   end
 
   # def search
